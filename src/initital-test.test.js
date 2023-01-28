c79d0544c20fd11a557b3f6852ea75e4e997cd62
lorena <lorena.anaya.diaz@gmail.com>
2023-01-28 12:29:51 +0100

    add yml to congifuration

--- src/initital-test.test.js
+++
@@ -1,5 +0,0 @@
-import { sum } from "./initial-test.js";
-test("given two values 2 and 3, when you add them together, then you get 5 ", () => {
-  const result = sum(2, 3);
-  expect(result).toBe(5);
-});
